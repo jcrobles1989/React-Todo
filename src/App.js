da82aabf766bd30da2a4174c264e1cce479ea394
Jose Carlos Robles <jcrobles1989@gmail.com>
2020-07-06 17:08:44 -0700

    set state in app, started on creating todolist and todo class components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,61 @@
 import React from 'react';
+import TodoList from './components/TodoList';
+import TodoForm from './components/TodoForm';
+
+const tasks = [
+  {
+    task: 'Organize Garage',
+    id: 1528817077286,
+    completed: false
+  },
+  {
+    task: 'Bake Cookies',
+    id: 1528817084358,
+    completed: false
+  }
+]
 
 class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      tasks: tasks
+    }
+  }
+
+  addTask = taskName => {
+    const newTask = {
+      task: taskName,
+      id: new Date.now(),
+      completed: false
+    };
+    this.setState({
+      tasks: [...this.state.tasks, newTask]
+    })
+  }
+
+  toggleTask = taskId => {
+    this.setState({
+      tasks: this.state.tasks.map(task => {
+        if (task.id === taskId) {
+          return {
+            ...task, 
+            completed: !task.completed
+          };
+        } else {
+          return task;
+        }
+      })
+    })
+  }
+
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
   render() {
     return (
       <div>
-        <h2>Welcome to your Todo App!</h2>
+        <h1>Todo List:</h1>
       </div>
     );
   }
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,2 +1,23 @@
+import React from 'react';
+import Todo from './Todo';
 // your components will all go in this `component` directory.
 // feel free to change this component.js into TodoList.js
+
+class TodoList {
+    constructor(props) {
+
+        return 
+    }
+
+    render() {
+        return (
+            <div className='tasks-list'>
+                {props.tasks.map(task => (
+                    <Todo toggleTask={props.toggleTask} key={task.id} item={task} />
+                ))}
+            </div>
+        )
+    }
+}
+
+export default TodoList;
